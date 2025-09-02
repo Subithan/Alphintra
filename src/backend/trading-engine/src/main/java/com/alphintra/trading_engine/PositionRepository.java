e4a13da8a1ecc62aefed882bcb58436ace209eac
daninithi <140165739+daninithi@users.noreply.github.com>
2025-09-02 12:27:08 +0530

    dani 2.09

--- src/backend/trading-engine/src/main/java/com/alphintra/trading_engine/PositionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alphintra.trading_engine;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PositionRepository extends JpaRepository<Position, Long> {
-}
--- src/backend/trading-engine/src/main/java/com/alphintra/trading_engine/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alphintra.trading_engine;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
